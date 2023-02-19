d996739035e1d0fb3daf1c1c641ce488aabc9c78
Jonathan Bihet <jonathan.bihet@gmail.com>
2023-02-19 11:22:12 +0100

    change console app to web app

--- Alfred/src/Alfred/Plugins/PluginPathFinder.cs
+++ Alfred/src/Alfred/Plugins/PluginPathFinder.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Logging;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 
 namespace Alfred.Plugins
 {
@@ -19,9 +20,12 @@ namespace Alfred.Plugins
         {            
             _logger = loggerFactory.CreateLogger<PluginPathFinder>();
             string basePath = config.GetValue<string>("PluginsPath") ?? string.Empty;
-            if (Directory.Exists(basePath))
+            string executingAssemblyPath = Assembly.GetExecutingAssembly().Location;
+            string executingDirectory = Path.GetDirectoryName(executingAssemblyPath) ?? ".";
+            string pluginsPath = Path.Combine(executingDirectory, basePath);
+            if (Directory.Exists(pluginsPath))
             {
-                foreach (string pluginPath in Directory.GetFiles(basePath).Where(path => Path.GetExtension(path).Equals(".dll")))
+                foreach (string pluginPath in Directory.GetFiles(pluginsPath).Where(path => Path.GetExtension(path).Equals(".dll")))
                 {
                     paths.Add(Path.GetFullPath(pluginPath));
                 }
